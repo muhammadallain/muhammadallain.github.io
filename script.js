cf4ebf79923243d7b8477659360d12cc6d3b4399
Muhammad Ali <96694357+muhammadallain@users.noreply.github.com>
2023-06-05 04:49:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function openTab(event, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+  }
+  
+  // Set the first tab as active by default
+  document.getElementsByClassName("tablink")[0].click();
+  
