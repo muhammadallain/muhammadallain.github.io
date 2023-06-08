b3090575f7b798e317ae793a22d15a16e0e22fea
Muhammad Ali <muhammadallain@gmail.com>
2023-06-08 04:36:42 +0100

    Tableau Project Upload

--- script.js
+++ script.js
@@ -15,3 +15,29 @@ function openTab(event, tabName) {
   // Set the first tab as active by default
   document.getElementsByClassName("tablink")[1].click();
   
+  var slides = document.querySelectorAll('.slide');
+var currentSlide = 0;
+
+function showSlide(n) {
+    slides[currentSlide].style.display = 'none';
+    currentSlide = (n + slides.length) % slides.length;
+    slides[currentSlide].style.display = 'block';
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Optional: Add event listeners for slideshow navigation
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.prev').addEventListener('click', prevSlide);
+    document.querySelector('.next').addEventListener('click', nextSlide);
+});
+
+// Show the first slide
+showSlide(currentSlide);
+
